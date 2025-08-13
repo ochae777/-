23fb0e61fc2b2e631d111fe7065de85416ceaf49
ochae777 <ochae777@yonsei.ac.kr>
2025-08-13 17:49:52 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+// 나누기
+function test3() {
+  n1 = document.querySelector(".test3 .n1").value;
+  n2 = document.querySelector(".test3 .n2").value;
+  document.querySelector(".test3 .result").textContent = n1 / n2;
+}
